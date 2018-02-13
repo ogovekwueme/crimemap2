3d8fabb0e9c8a86987d7cdec15c51f6ede2151ae
=Victor Ekwueme <=victor.ekwueme@gmail.com>
2018-02-13 13:47:09 +0100

    adjusting dates

--- crimemap.py
+++ crimemap.py
@@ -1,4 +1,6 @@
 import json
+import datetime
+import dateparser
 import dbconfig
 from flask import Flask, render_template, request
 
@@ -11,12 +13,23 @@ app = Flask(__name__)
 
 DB = DBHelper()
 
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.datetime.strftime(date, "%Y-%m-%d")
+    except TypeError:
+        return None
+
+categories = ['mugging', 'break-in']
+
 @app.route("/")
-@app.route("/")
-def home():
+def home(error_message=None):
     crimes = DB.get_all_crimes()
     crimes = json.dumps(crimes)
-    return render_template("home.html", crimes=crimes)
+    return render_template("home.html", crimes=crimes,
+        categories=categories, error_message=error_message)
+
+
 
 @app.route("/add", methods=["POST"])
 def add():
@@ -38,9 +51,16 @@ def clear():
 @app.route("/submitcrime", methods=['POST'])
 def submitcrime():
     category = request.form.get("category")
-    date = request.form.get("date")
-    latitude = float(request.form.get("latitude"))
-    longitude = float(request.form.get("longitude"))
+    if category not in categories:
+        return home()
+    date = format_date(request.form.get("date"))
+    if not date:
+        return home("Invalid date. Please use yyyy-mm-dd format")
+    try:
+        latitude = float(request.form.get("latitude"))
+        longitude = float(request.form.get("longitude"))
+    except ValueError:
+        return home()
     description = request.form.get("description")
     DB.add_crime(category, date, latitude, longitude, description)
     return home()
